9baa10f6eca78a2770682f2f5ae726e0b4deebf0
Joe Sadoski <jdsadoski@gmail.com>
2021-04-25 03:17:40 -0400

    Navbar styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar class="navbar is-warning">
       <template #brand>
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
           <img
-            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
+            :src="require('~/assets/images/Oscars_2021_logo.png')"
+            alt="The Oscars 2021 Logo"
           />
         </b-navbar-item>
       </template>
-      <template #start>
-        <b-navbar-item href="#"> Home </b-navbar-item>
-        <b-navbar-item href="#"> Documentation </b-navbar-item>
-        <b-navbar-dropdown label="Info">
-          <b-navbar-item href="#"> About </b-navbar-item>
-          <b-navbar-item href="#"> Contact </b-navbar-item>
-        </b-navbar-dropdown>
-      </template>
+      <template #start> </template>
 
-      <template #end>
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </b-navbar-item>
-      </template>
+      <template #end> </template>
     </b-navbar>
 
     <section class="main-content columns">
